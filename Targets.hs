6258f9ae12e0c64b9f025f1fc6544f43e548c13d
David Fox <dsf@seereason.com>
2009-03-25 17:40:34 +0000

    Disable tagsoup target, we can use the upstream version now

--- Targets.hs
+++ Targets.hs
@@ -297,9 +297,10 @@ otherTargets =
              , sourceSpec = "apt:sid:jquery"
              , relaxInfo = [] 
              }
+{-  We no longer need our own version of this.
     , Target { sourcePackageName = "haskell-tagsoup"
              , sourceSpec = "deb-dir:(uri:http://hackage.haskell.org/packages/archive/tagsoup/0.6/tagsoup-0.6.tar.gz:4db416463189292609b4cd3d0e867ccd):(darcs:http://src.seereason.com/debian/tagsoup-debian)"
-             , relaxInfo = [] }
+             , relaxInfo = [] } -}
     ]
 
 privateTargets =
