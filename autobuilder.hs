a76a8ea5f1d433bb799b15272b48cbf47d6e8327
David Fox <dsf@seereason.com>
2012-10-26 19:34:47 +0000

    Require autobuilder 6.35 for the new cabal-debian

--- autobuilder.hs
+++ autobuilder.hs
@@ -259,7 +259,8 @@ defParams _home myBuildRelease =
     -- 6.14 adds the ExtraDevDep parameter.
     -- 6.15 changes Epoch parameter arity to 2
     -- 6.18 renames type Spec -> RetrieveMethod
-    , requiredVersion = [(parseDebianVersion "6.28", Nothing)]
+    -- 6.35 added the CabalDebian flag
+    , requiredVersion = [(parseDebianVersion "6.35", Nothing)]
     , hackageServer = myHackageServer
     -- Things that are probably obsolete
     , debug = False
