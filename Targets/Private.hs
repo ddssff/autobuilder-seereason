7305501e370e85975522d587489d766f0ebd727c
David Fox <dsf@seereason.com>
2013-03-03 15:07:29 +0000

    Use new combinators for the private targets

--- Targets/Private.hs
+++ Targets/Private.hs
@@ -15,49 +15,22 @@ import Targets.Common
 
 libraries _home =
     P.Packages (singleton "libraries") $
-    [ P.Package { P.name = "haskell-generic-formlets3"
-                , P.spec = Darcs (privateRepo ++ "/generic-formlets3")
-                , P.flags = [] }
-{-  , P.Package { P.name = "haskell-document"
-                , P.spec = Darcs (privateRepo ++ "/haskell-document")
-                , P.flags = [] } -}
-    , P.Package { P.name = "haskell-ontology"
-                , P.spec = Darcs (privateRepo ++ "/haskell-ontology")
-                , P.flags = [] }
+    [ darcs "haskell-generic-formlets3" (privateRepo ++ "/generic-formlets3")
+    -- , darcs "haskell-document" (privateRepo ++ "/haskell-document")
+    , darcs "haskell-ontology" (privateRepo ++ "/haskell-ontology")
     ] ++ clckwrks14
 
 applications _home =
     P.Packages (singleton "applications") $
-    [ P.Package { P.name = "haskell-artvaluereport2"
-                , P.spec = Debianize (Darcs (privateRepo ++ "/artvaluereport2"))
-                , P.flags = [] }
-{-  , P.Package { P.name = "haskell-artvaluereport"
-                , P.spec = Darcs (privateRepo ++ "/artvaluereport")
-                , P.flags = [] }
-    , P.Package { P.name = "appraisalreportonline"
-                , P.spec = Debianize (Darcs (privateRepo ++ "/appraisalreportonline"))
-                , P.flags = [] } -}
-    , P.Package { P.name = "haskell-artvaluereport-data"
-                , P.spec = Debianize (Darcs (privateRepo ++ "/artvaluereport-data"))
-                , P.flags = [P.ExtraDep "haskell-hsx-utils"] }
-{-  , P.Package { P.name = "haskell-happstack-mailinglist"
-                , P.spec = Darcs (privateRepo ++ "/mailingList")
-                , P.flags = [] } -}
-    , P.Package { P.name = "haskell-seereason"
-                , P.spec = Darcs (privateRepo ++ "/seereason")
-                , P.flags = [] }
-    , P.Package { P.name = "haskell-happstack-ontology"
-                , P.spec = Darcs (privateRepo ++ "/happstack-ontology")
-                , P.flags = [] }
-    , P.Package { P.name = "haskell-creativeprompts"
-                , P.spec = Darcs (privateRepo ++ "/creativeprompts")
-                , P.flags = [] }
+    [ debianize (darcs "haskell-artvaluereport2" (privateRepo ++ "/artvaluereport2"))
+    , debianize (darcs "haskell-artvaluereport-data" (privateRepo ++ "/artvaluereport-data"))
+    , darcs "haskell-seereason" (privateRepo ++ "/seereason")
+    , darcs "haskell-happstack-ontology" (privateRepo ++ "/happstack-ontology")
+    , darcs "haskell-creativeprompts" (privateRepo ++ "/creativeprompts")
     -- There is a debianization in the repo that contains this file
     -- (Targets.hs), and it creates a package named seereason-darcs-backups,
     -- which performs backups on the darcs repo.
-    , P.Package { P.name = "seereason-darcs-backups"
-                , P.spec = Darcs (repo ++ "/autobuilder-config")
-                , P.flags = [] }
+    , darcs "seereason-darcs-backups" (repo ++ "/autobuilder-config")
 {-
     , P.Package { P.name = "clcksmith"
                 , P.spec = Debianize (Darcs (privateRepo ++ "/clcksmith"))
@@ -69,18 +42,17 @@ applications _home =
                 -- Haddock gets upset about the HSX.QQ modules.  Not sure why.
                 , P.flags = [P.ExtraDep "haskell-hsx-utils", P.NoDoc] }
 -}
-    , P.Package { P.name = "seereasonpartners-dot-com"
-                , P.spec = Debianize (Cd "seereasonpartners-dot-com" (Darcs (privateRepo ++ "/seereasonpartners-clckwrks")))
-                , P.flags = [] }
-    , P.Package { P.name = "haskell-clckwrks-theme-seereasonpartners"
-                , P.spec = Debianize (Cd "clckwrks-theme-seereasonpartners" (Darcs (privateRepo ++ "/seereasonpartners-clckwrks")))
-                , P.flags = [P.ExtraDep "haskell-hsx-utils", P.NoDoc] }
-    , P.Package { P.name = "appraisalreportonline-dot-com"
-                , P.spec = Debianize (Cd "appraisalreportonline-dot-com" (Darcs (privateRepo ++ "/appraisalreportonline-clckwrks")))
-                , P.flags = [] }
-    , P.Package { P.name = "haskell-clckwrks-theme-appraisalreportonline"
-                , P.spec = Debianize (Cd "clckwrks-theme-appraisalreportonline" (Darcs (privateRepo ++ "/appraisalreportonline-clckwrks")))
-                , P.flags = [P.ExtraDep "haskell-hsx-utils"] }
+    , debianize (method "seereasonpartners-dot-com"
+                      (Cd "seereasonpartners-dot-com" (Darcs (privateRepo ++ "/seereasonpartners-clckwrks"))))
+    , debianize (method "haskell-clckwrks-theme-seereasonpartners"
+                      (Cd "clckwrks-theme-seereasonpartners" (Darcs (privateRepo ++ "/seereasonpartners-clckwrks")))
+                   `flag` P.ExtraDep "haskell-hsx-utils"
+                   `flag` P.NoDoc)
+    , debianize (method "appraisalreportonline-dot-com"
+                      (Cd "appraisalreportonline-dot-com" (Darcs (privateRepo ++ "/appraisalreportonline-clckwrks"))))
+    , debianize (method "haskell-clckwrks-theme-appraisalreportonline"
+                      (Cd "clckwrks-theme-appraisalreportonline" (Darcs (privateRepo ++ "/appraisalreportonline-clckwrks")))
+                   `flag` P.ExtraDep "haskell-hsx-utils")
     ]
 
 extraSplits =
