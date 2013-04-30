c74776babc9ea723669c28dc385e116050bb9b48
David Fox <dsf@seereason.com>
2013-04-30 18:46:29 +0000

    Add locales to packages required to create build environment

--- Debian/AutoBuilder/Details.hs
+++ Debian/AutoBuilder/Details.hs
@@ -265,6 +265,7 @@ myIncludePackages myBuildRelease =
     , "build-essential"         -- This is required by autobuilder code that opens the essential-packages list
     , "pkg-config"              -- Some packages now depend on this package via new cabal options.
     , "debian-keyring"
+    , "locales"
     -- , "seereason-keyring"
     -- , "perl-base"
     -- , "gnupg"
