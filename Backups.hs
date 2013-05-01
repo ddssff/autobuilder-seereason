103a9af17432ec998097a62d02757f70944e0411
David Fox <dsf@seereason.com>
2013-05-01 23:10:24 +0000

    Fix authentication info for backup process

--- Backups.hs
+++ Backups.hs
@@ -4,8 +4,8 @@ import Network.URI (URIAuth(..))
 import System.Archive.Site (BackupTarget(..), backup)
 
 main = backup (BackupTarget { app = "darcs"
-                            , auth = URIAuth { uriUserInfo = "autobuilder@"
-                                             , uriRegName = "deb.seereason.com"
+                            , auth = URIAuth { uriUserInfo = "upload@"
+                                             , uriRegName = "src.seereason.com"
                                              , uriPort = "" }
                             , localTop = "/home/autobuilder/backups"
                             , remoteTop = "/srv"
