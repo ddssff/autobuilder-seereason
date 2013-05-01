0bffbed8f15c457eb802fc6731081f90b506b6c5
David Fox <dsf@seereason.com>
2013-05-01 19:49:41 +0000

    Update the /srv/darcs backup script for archive-1.4

--- Backups.hs
+++ Backups.hs
@@ -2,4 +2,10 @@ module Main where
 
 import System.Archive.Site (BackupTarget(..), backup)
 
-main = backup (BackupTarget {app = "darcs", user = "upload", host = "src.seereason.com", keep = 50})
+main = backup (BackupTarget { app = "darcs"
+                            , auth = URIAuth { uriUserInfo = "autobuilder@"
+                                             , uriRegName = "deb.seereason.com"
+                                             , uriPort = "" }
+                            , localTop = "/home/autobuilder/backups"
+                            , remoteTop = "/srv"
+                            , keep = 50 })
