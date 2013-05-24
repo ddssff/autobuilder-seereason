1307cc35f313e7e2f57b54d8db939e74b2a13ee1
David Fox <dsf@seereason.com>
2013-05-24 22:20:22 +0000

    Change autobuilder email to partners@seereason.com

--- Debian/AutoBuilder/Details.hs
+++ Debian/AutoBuilder/Details.hs
@@ -32,7 +32,7 @@ myParams home myBuildRelease =
     ParamRec
     { vendorTag = myVendorTag
     , oldVendorTags = ["seereason"]
-    , autobuilderEmail = "SeeReason Autobuilder <autobuilder@seereason.com>"
+    , autobuilderEmail = "SeeReason Autobuilder <partners@seereason.com>"
     , releaseSuffixes = myReleaseSuffixes
     , buildRelease = ReleaseName {relName = myBuildRelease}
     , uploadURI = myUploadURI myBuildRelease
