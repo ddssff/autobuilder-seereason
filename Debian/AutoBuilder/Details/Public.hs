5f0aee10ca21088a3401e4300f49c7caeb4b921f
David Fox <dsf@seereason.com>
2014-06-03 02:39:52 +0000

    Updates for new lens package, new ghcjs package, autobuilder-seereason library

--- Debian/AutoBuilder/Details/Public.hs
+++ Debian/AutoBuilder/Details/Public.hs
@@ -1180,7 +1180,7 @@ algebra release =
     -- lens-4.0 depends on aeson >= 0.7, which is not in hackage yet.  Also, lens-3.10.2 depends on a version of
     -- monadcatchio-transformers<0.3.1, which is older than our oldest.
     , case ghc release of
-        708 -> debianize (hackage "lens" `patch` $(embedFile "patches/lens4.diff"))
+        708 -> debianize (hackage "lens" {- `patch` $(embedFile "patches/lens4.diff") -})
         _ -> debianize (hackage "lens" `patch` $(embedFile "patches/lens.diff") `flag` P.CabalPin "3.10.1")
     , debianize (hackage "constraints")
     , debianize (hackage "lens-family-core")
