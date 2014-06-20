5a64467bc886f8912bf6a8758ef801b6da5fbf42
David Fox <dsf@seereason.com>
2014-06-20 21:56:09 +0000

    Relax a dependency in ghcjs-dom-hello

--- Debian/AutoBuilder/Details/Public.hs
+++ Debian/AutoBuilder/Details/Public.hs
@@ -1343,7 +1343,9 @@ ghcjs release =
                  `flag` P.BuildDep "ghcjs"
                  `flag` P.BuildDep "haskell-devscripts (>= 0.8.21-3)")
   , debianize (hackage "ghcjs-dom-hello"
+                 `patch` $(embedFile "patches/ghcjs-dom-hello.diff")
                  `flag` P.CabalDebian ["--hc=ghcjs"]
+                 `flag` P.CabalDebian ["--default-package=ghcjs-dom-hello"]
                  `flag` P.BuildDep "ghcjs"
                  `flag` P.BuildDep "haskell-devscripts (>= 0.8.21-3)")
   -- , debianize (git "ghcjs-base" "https://github.com/ghcjs/ghcjs-base")
