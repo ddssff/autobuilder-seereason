9f31892d23c78492ce994c4d6147f5d0a3534f6c
David Fox <dsf@seereason.com>
2018-04-10 14:57:48 -0700

    Switch xenial-seereason7 from a ghc-7.10 build to a build with ghc-8.0.2 and the ghc-8.0 branch of ghcjs

--- Debian/AutoBuilder/Details/Xenial.hs
+++ Debian/AutoBuilder/Details/Xenial.hs
@@ -38,6 +38,25 @@ buildTargets7 :: Monad m => TSt m ()
 buildTargets7 = do
   _ghcjs <- git "https://github.com/ddssff/ghcjs-debian" [] >>= inGroups ["ghcjs-comp", "ghcjs-only"]
   _nodejs <- nodejs
+  _old_locale <- hackage (Just "1.0.0.7") "old-locale" >>= flag (P.DebVersion "1.0.0.7-2") >>= debianize []
+  _old_time <- hackage (Just "1.1.0.3") "old-time" >>= flag (P.DebVersion "1.1.0.3-2") >>= debianize []
+  _cabal_install <- hackage (Just "1.24.0.2") "cabal-install" >>=
+                    -- Avoid creating a versioned libghc-cabal-dev
+                    -- dependency, as it is a virtual package in ghc
+                    patch $(embedFile "patches/cabal-install.diff") >>=
+                    debianize [] >>=
+                    -- Allow building with Cabal-2
+                    flag (P.CabalDebian ["--default-package", "cabal-install"]) >>=
+                    inGroups []
+  _haskell_devscripts <-
+      -- Revert to version we used from 8/2016-11/2016
+      git "http://anonscm.debian.org/cgit/pkg-haskell/haskell-devscripts.git" [Commit "a143f70d333663e1447998d6facbebf67cd5045f"] >>=
+      -- This changes --show-details=direct to --show-details=always in check-recipe
+      patch $(embedFile "patches/haskell-devscripts.diff") >>=
+      flag (P.RelaxDep "python-minimal") >>= inGroups ["platform"]
+  -- _ghc_boot <- hackage (Just "8.0.1") "ghc-boot" >>= debianize [] -- Required by haddock-api
+  -- traverse-with-class-1.0.0.0 requires base >= 4.9, aka ghc8.  This blocks haskell-names-0.9.
+#if 0
   -- _binary <- hackage (Just "0.8.4.0") "binary" >>= debianize [] >>= ghcjs_also
   _haddock_api7 <-
       hackage (Just "2.16.1") "haddock-api" >>=
@@ -50,8 +69,6 @@ buildTargets7 = do
                                                                   , "DEB_SETUP_GHC_CONFIGURE_ARGS = --constraint=Cabal==$(shell dpkg -L ghc | grep 'package.conf.d/Cabal-' | sed 's/^.*Cabal-\\([^-]*\\)-.*$$/\\1/')\n"])) >>=
              debianize [] >>= inGroups ["ghcjs-comp"]
   _haddock_library7 <- hackage (Just "1.2.1") "haddock-library" >>= flag (P.DebVersion "1.2.1-2") >>= debianize [] >>= ghcjs_also
-  _old_locale <- hackage (Just "1.0.0.7") "old-locale" >>= flag (P.DebVersion "1.0.0.7-2") >>= debianize []
-  _old_time <- hackage (Just "1.1.0.3") "old-time" >>= flag (P.DebVersion "1.1.0.3-2") >>= debianize []
   _ghcjs_dom <- hackage (Just "0.2.4.0" {-"0.7.0.4"-} {-"0.4.0.0"-}) "ghcjs-dom" >>= debianize [] >>= inGroups ["glib"] >>= ghcjs
   _ghcjs_dom_hello <- hackage (Just "2.0.0.0") "ghcjs-dom-hello" >>=
                       patch $(embedFile "patches/ghcjs-dom-hello.diff") >>=
@@ -60,19 +77,43 @@ buildTargets7 = do
                       inGroups ["glib"] >>=
                       ghcjs >>=
                       skip (Reason "see cairo and glib")
-  _haskell_devscripts <-
-      -- Revert to version we used from 8/2016-11/2016
-      git "http://anonscm.debian.org/cgit/pkg-haskell/haskell-devscripts.git" [Commit "a143f70d333663e1447998d6facbebf67cd5045f"] >>=
-      -- This changes --show-details=direct to --show-details=always in check-recipe
-      patch $(embedFile "patches/haskell-devscripts.diff") >>=
-      flag (P.RelaxDep "python-minimal") >>= inGroups ["platform"]
-  -- _ghc_boot <- hackage (Just "8.0.1") "ghc-boot" >>= debianize [] -- Required by haddock-api
-  -- traverse-with-class-1.0.0.0 requires base >= 4.9, aka ghc8.  This blocks haskell-names-0.9.
   _traverse_with_class <- hackage (Just "0.2.0.4") "traverse-with-class" >>= debianize [] >>= inGroups ["happstack", "important"]
   -- Unfortunately this package requires haskell-src-exts<1.19, which is gone
   _haskell_names <- hackage (Just "0.8.0") "haskell-names" >>= debianize []
   _singletons <- hackage (Just "2.1") "singletons" >>= debianize []
   _uri_bytestring <- hackage (Just "0.3.0.2") "uri-bytestring" >>= debianize [] >>= inGroups ["servant"] >>= ghcjs_also
+#else
+  -- ghc-8.0.2-11
+  _ghc8 <- apt "buster" "ghc" >>= patch $(embedFile "patches/ghc.diff") >>= inGroups ["ghc8-comp"]
+  _ghcjs <- git "https://github.com/ddssff/ghcjs-debian" [Branch "ghc-8.0"] >>= inGroups ["ghcjs-comp", "ghcjs-only"]
+  _haddock_api8 <-
+      hackage (Just "2.17.4") "haddock-api" >>=
+             flag (P.CabalDebian ["--default-package", "haddock-api"]) >>=
+             flag (P.CabalDebian ["--missing-dependency", "libghc-cabal-dev"]) >>=
+             flag (P.CabalDebian ["--missing-dependency", "libghc-cabal-prof"]) >>=
+{-
+             -- This breaks the build
+             apply (execCabalM $ (debInfo . rulesFragments) %=
+                                         Set.insert (Text.unlines [ "# Force the Cabal dependency to be the version provided by GHC"
+                                                                  , "DEB_SETUP_GHC_CONFIGURE_ARGS = --constraint=Cabal==$(shell dpkg -L ghc | grep 'package.conf.d/Cabal-' | sed 's/^.*Cabal-\\([^-]*\\)-.*$$/\\1/')\n"])) >>=
+-}
+             debianize [] >>= inGroups ["ghcjs-comp"]
+  _haddock_library8 <- hackage (Just "1.4.2") "haddock-library" >>= debianize [] >>= ghcjs_also
+  _ghcjs_dom <- hackage (Just "0.9.1.1") "ghcjs-dom" >>= debianize [] >>= inGroups ["glib"] >>= ghcjs
+  _ghcjs_dom_hello <- hackage (Just "6.0.0.0") "ghcjs-dom-hello" >>=
+                      patch $(embedFile "patches/ghcjs-dom-hello.diff") >>=
+                      flag (P.CabalDebian ["--default-package", "ghcjs-dom-hello"]) >>=
+                      debianize [] >>=
+                      inGroups ["glib"] >>=
+                      ghcjs >>=
+                      skip (Reason "see cairo and glib")
+  _traverse_with_class <- hackage (Just "1.0.0.0") "traverse-with-class" >>= debianize [] >>= inGroups ["happstack", "important"]
+  _haskell_names <- hackage (Just "0.9.1") "haskell-names" >>= debianize []
+  _singletons <- hackage (Just "2.3.1") "singletons" >>= debianize [] -- 2.4 requires base-4.11
+  -- The ghc-8.0 branch of ghcjs needs version 0.3.1.0, but we
+  -- are not using that yet.
+  _uri_bytestring <- hackage (Just "0.3.0.2") "uri-bytestring" >>= debianize [] >>= inGroups ["servant"] >>= ghcjs_also
+#endif
   buildTargets
 
 nodejs :: Monad m => TSt m PackageId
